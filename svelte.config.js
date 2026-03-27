8db6bc50b7d947da579adb323551454f2423eb3c
ahmedalomary1970 <ahmedalomary@hotmail.com>
2026-03-27 06:48:15 +0300

    switch to vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
-import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
   preprocess: vitePreprocess(),
+
   kit: {
-    adapter: adapter({
-      fallback: "index.html"
-    })
+    adapter: adapter()
   }
 };
 
